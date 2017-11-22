aff70213623dad6c4c6bdd13eb5c2467baffb72c
KeyvanDerakhshanNik <kdn807@mun.ca>
2017-11-22 14:34:33 -0330

    Create Project For Complex Network

---
+++ ComplexNetworkProject1/src/complexnetworkproject1/ComplexNetworkProject1.java
@@ -0,0 +1,21 @@
+/*
+ * AirPort Data Retrival 
+ * 
+ */
+package complexnetworkproject1;
+
+/**
+ *
+ * @author Keyvan
+ */
+public class ComplexNetworkProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+    }
+    
+}
