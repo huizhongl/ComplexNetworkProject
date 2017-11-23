2eef3ed14953c37ca2bfe91bdf8afc92eb64a102
KeyvanDerakhshanNik <kdn807@mun.ca>
2017-11-23 17:21:52 -0330

    Step one of Project. Data Mining

---
+++ ComplexNetwork1/src/complexnetwork2/Airport.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complexnetwork2;
+
+/**
+ *
+ * @author Keyvan
+ */
+public class Airport {
+    
+    private String cName;
+    private String Country;
+    
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+}
